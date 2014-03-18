5f31271e017cc3db60e4ebf4ad9914cd70ec1051
bionicsheep <alessandroeppacher@gmail.com>
2014-03-18 12:43:47 -0400

    Initial Commit

---
+++ src/me/bionicsheep/paimc/MainActivity.java
@@ -0,0 +1,77 @@
+package me.bionicsheep.paimc;
+import me.bionicsheep.paimc.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.widget.Toast;
+
+public class MainActivity extends PreferenceActivity {
+
+	SharedPreferences sp;
+	SharedPreferences.Editor editor;
+	CheckBoxPreference service_checkbox;
+	Activity currentActivity = this;
+	Toast toast;
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.layout.main_preference);
+
+		loadValues();
+		initializeObjects();
+	}
+
+	@SuppressWarnings("deprecation")
+	public void initializeObjects(){
+		service_checkbox = (CheckBoxPreference)getPreferenceManager().findPreference("service_checkbox");
+		service_checkbox.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				startPie((Boolean) newValue);
+				return true;
+			}
+		});
+		startPie(sp.getBoolean("service_status", false));
+	}
+
+	public void startPie(boolean state){
+		editor = sp.edit();
+
+		if (state == true) {
+			startService(new Intent(currentActivity, TriggerService.class));
+			editor.putBoolean("service_status", true);
+			toast = Toast.makeText(currentActivity, "Service Running", Toast.LENGTH_SHORT);
+			toast.show();
+		} else {
+			stopService(new Intent(currentActivity, TriggerService.class));
+			editor.putBoolean("service_status", false);
+			toast = Toast.makeText(currentActivity, "Service Not Running", Toast.LENGTH_SHORT);
+			toast.show();
+		}
+
+		editor.commit();
+	}
+
+	public void loadValues(){
+		sp = getSharedPreferences("app_settings", MODE_PRIVATE);
+		
+		boolean firstRun = sp.getBoolean("firstrun", true);
+		if(firstRun){
+			toast = Toast.makeText(this, "First Run", Toast.LENGTH_LONG);
+			toast.show();
+			enterValues();
+		}
+	}
+
+	public void enterValues(){
+		editor = sp.edit();
+		editor.putBoolean("firstrun", false);
+		editor.putBoolean("service_status", false);
+		editor.commit();
+	}
+}
---
+++ src/me/bionicsheep/paimc/TriggerService.java
@@ -0,0 +1,164 @@
+package me.bionicsheep.paimc;
+import me.bionicsheep.paimc.R;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class TriggerService extends Service{
+
+	ImageView detectorArea, background;
+	WindowManager wm;
+	WindowManager.LayoutParams tparams, bparams;
+	int twidth, theight;
+	int bwidth, bheight;
+
+	Display display;
+	int displayWidth, displayHeight;
+
+	boolean scanning = true;
+	int dragY;
+	int shadow_threshold;
+
+	Service currentActivity = this;
+	Handler handler;
+	Toast toast;
+
+	Canvas canvas;
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// We want this service to continue running until it is explicitly
+		// stopped, so return sticky.
+		return START_STICKY;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onCreate(){
+		detectorArea = new ImageView(this);
+		background = new ImageView(this);
+
+		handler = new Handler();
+
+		wm = (WindowManager) getSystemService(WINDOW_SERVICE);		
+		display = wm.getDefaultDisplay();
+
+		displayWidth = display.getWidth();
+		displayHeight = display.getHeight();
+
+		startTrigger();
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private OnTouchListener triggerTouchListener = new OnTouchListener(){
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			if(event.getAction() == MotionEvent.ACTION_DOWN){
+				startBackground();
+			}else if(event.getAction() == MotionEvent.ACTION_UP){
+				scanning = true;
+				wm.removeView(background);
+			}else if(event.getAction() == MotionEvent.ACTION_MOVE){
+				dragY = (int) -event.getY();
+				if(scanning && dragY > shadow_threshold){
+					scanning = false;
+					fadeToDark();
+				}
+			}
+			return true;
+		}
+	};
+
+	@Override
+	public void onDestroy(){
+		if(detectorArea != null) { 
+			wm.removeView(detectorArea);
+		}
+		super.onDestroy();
+	}
+
+	private void fadeToDark(){
+		(new Thread(){
+			int n = 0;
+			public void run(){
+
+				for(n = 0; n < 175; n++){
+					try{
+						runOnUiThread(new Runnable(){
+							@Override
+							public void run() {
+								background.setBackgroundColor(Color.argb(n, n, 0, 0));
+							}
+						});
+						sleep(3);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+	}
+
+	private void startTrigger(){
+		detectorArea.setImageResource(R.drawable.detector);
+		detectorArea.setOnTouchListener(triggerTouchListener);
+		detectorArea.setScaleType(ImageView.ScaleType.FIT_XY);
+		twidth = displayWidth / 2;
+		theight = 10;
+		shadow_threshold = displayHeight / 3;
+
+		tparams = new WindowManager.LayoutParams(
+				twidth,
+				theight,
+				WindowManager.LayoutParams.TYPE_PHONE,
+				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+				PixelFormat.TRANSLUCENT
+				);
+
+		tparams.gravity = Gravity.CENTER | Gravity.BOTTOM;
+
+		wm.addView(detectorArea, tparams);
+	}
+
+	private void startBackground(){
+		bwidth = displayWidth;
+		bheight = displayHeight;
+		Log.d("display","height " + displayHeight);
+
+		bparams = new WindowManager.LayoutParams(
+				bwidth,
+				bheight,
+				WindowManager.LayoutParams.TYPE_PHONE,
+				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+				PixelFormat.TRANSPARENT
+				);
+
+		bparams.gravity = Gravity.CENTER | Gravity.BOTTOM;
+
+		wm.addView(background, bparams);
+		background.setBackgroundColor(Color.argb(0, 0, 0, 0));
+	}
+
+	private void runOnUiThread(Runnable runnable) {
+		handler.post(runnable);
+	}
+
+}
